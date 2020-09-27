359eb65b52997bfed05b4d56ee48fdb685cc9216
Err0-R <71987359+Err0-R@users.noreply.github.com>
2020-09-27 23:08:45 +0300

    Add files via upload

---
+++ Loop.py
@@ -0,0 +1,51 @@
+import os
+os.system("clear")
+
+lol = input("Enter Password: ")
+
+if lol == "error&&" : 
+    
+    os.system("clear")
+                                     
+    print("LLLLLLLLLLL                  OOOOOOOOO          OOOOOOOOO     PPPPPPPPPPPPPPPPP   ")
+    print("L:::::::::L                OO:::::::::OO      OO:::::::::OO   P::::::::::::::::P  ")
+    print("L:::::::::L              OO:::::::::::::OO  OO:::::::::::::OO P::::::PPPPPP:::::P ")
+    print("LL:::::::LL             O:::::::OOO:::::::OO:::::::OOO:::::::OPP:::::P     P:::::P ")
+    print("  L:::::L               O::::::O   O::::::OO::::::O   O::::::O  P::::P     P:::::P ")
+    print("  L:::::L               O:::::O     O:::::OO:::::O     O:::::O  P::::P     P:::::P ")
+    print("  L:::::L               O:::::O     O:::::OO:::::O     O:::::O  P::::PPPPPP:::::P ")
+    print("  L:::::L               O:::::O     O:::::OO:::::O     O:::::O  P:::::::::::::PP  ")
+    print("  L:::::L               O:::::O     O:::::OO:::::O     O:::::O  P::::PPPPPPPPP    ")
+    print("  L:::::L               O:::::O     O:::::OO:::::O     O:::::O  P::::P            ")
+    print("  L:::::L               O:::::O     O:::::OO:::::O     O:::::O  P::::P            ")
+    print("  L:::::L         LLLLLLO::::::O   O::::::OO::::::O   O::::::O  P::::P            ")
+    print("LL:::::::LLLLLLLLL:::::LO:::::::OOO:::::::OO:::::::OOO:::::::OPP::::::PP          ")
+    print("L::::::::::::::::::::::L OO:::::::::::::OO  OO:::::::::::::OO P::::::::P          ")
+    print("L::::::::::::::::::::::L   OO:::::::::OO      OO:::::::::OO   P::::::::P          ")
+    print("LLLLLLLLLLLLLLLLLLLLLLLL     OOOOOOOOO          OOOOOOOOO     PPPPPPPPPP         ")
+    
+    print("")
+    print("")
+    print("                  Follow me on YouTube")
+    print("https://www.youtube.com/channel/UCgKHX52K5ySB-BpXnLIQQlw")
+    print("")
+    print("")
+    
+    x = input("Type Text or number: ")
+        
+    xx = input("900K To Continue (Y) to exit (N)")
+    
+    if xx == "Y" : 
+        print(x *900000)
+        
+    if xx == "N" :
+        os.system("clear")
+        os.system("exit")
+        
+    else :
+        print("wrong word") 
+        
+else :
+    print ("wrong password")
+
+    
---
+++ README.py
@@ -0,0 +1,9 @@
+import os
+os.system("clear")
+print("                       Err0-R")
+print("")
+print("This tool is used to print a word of your choice 900,000 times")
+print("")
+print("")
+print("              folow me on YouTube") 
+print("https://www.youtube.com/channel/UCgKHX52K5ySB-BpXnLIQQlw")
